1cae28f00781f184a12f27158f086e6b521ee49b
vinh15me <vinh15me@outlook.com>
2025-02-13 20:21:38 -0800

    Finished

---
+++ cmake-build-debug/CMakeFiles/LinkedList210.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinkedList210.dir/main.cpp.obj"
+  "CMakeFiles/LinkedList210.dir/main.cpp.obj.d"
+  "LinkedList210.exe"
+  "LinkedList210.exe.manifest"
+  "LinkedList210.pdb"
+  "libLinkedList210.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinkedList210.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
